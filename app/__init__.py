9188337b2f5c477f9042880275b083435775d212
prakhar060703 <99869705+prakhar060703@users.noreply.github.com>
2025-07-14 18:53:19 +0530

    Updated __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -10,17 +10,33 @@ def create_app():
     
     # SQLite setup
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/app.db'
+    # If the file doesn’t exist, SQLite automatically creates it when you connect.
+    # If it does exist, it just uses the existing database file.
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # It tells Tells SQLAlchemy not to track every change to objects in memory — which saves memory and avoids warnings.
 
     db.init_app(app)
+    # I created db = SQLAlchemy() outside the app (global scope).
+    # But Flask needs it to be connected to a specific app instance.
+    # So calling db.init_app(app) connects SQLAlchemy to the Flask app created by create_app().
 
     # Create DB
     with app.app_context():
         from .models import models
         db.create_all()
 
+    # Flask uses something called an "application context" to keep track of which app is currently running.
+    # Some operations (like database access) only work inside this context.
+    # db.create_all()
+    # This tells SQLAlchemy to:
+    # “Look at all models I imported. If the tables for them don’t exist in the database yet, create them.”
+
     # Register blueprints
     from .controllers.auth_controller import auth_bp
     app.register_blueprint(auth_bp)
+    # Think of auth_bp as a mini-app that contains login, signup, and maybe logout routes — all grouped together.
+    # It’s like saying:
+    # “Hey Flask, include all the routes and logic defined in this blueprint.”
 
     return app
